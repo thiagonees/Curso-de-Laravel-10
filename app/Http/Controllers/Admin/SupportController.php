fb1d0c182ccec14119e18ea982d1ed578c4c3d95
d-berbattov <berbattov-tech@gmail.com>
2024-06-10 21:05:31 -0300

    🗃️ Create support migration

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Support;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index() {
-        return view('admin/supports/index');
+    public function index(Support $support): \Illuminate\Contracts\View\Factory|\Illuminate\Foundation\Application|\Illuminate\Contracts\View\View|\Illuminate\Contracts\Foundation\Application
+    {
+
+        $support = new Support();
+        $supports = $support->all(); //retorna  collectiion (array)
+
+
+
+        return view('admin/supports/index', compact('supports'));
     }
 }
